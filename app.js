23c96968ce1494143ead841480f1cd0b5f5ba362
Автомонова Дар'я Сергіївна <104759810+Kh1merA@users.noreply.github.com>
2024-03-05 16:36:12 +0200

    complete hw3

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ import logger from './lib/logger/logger.js';
 import {add} from './handler.js';
 
 const log = logger.getLogger('app.js');
+const data = {"MVP amount": 11};
+const user = {"name": "Alex", "team": "NAVI"};
 
 log.info(color);
+log.info("Data", data, "User:", user, "S1mple is the best");
 log.warn(fruit);
 log.error("ERROR occur: My log");
 log.debug(color);
